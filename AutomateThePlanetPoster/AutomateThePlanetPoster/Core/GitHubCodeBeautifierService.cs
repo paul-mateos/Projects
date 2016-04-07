9166288194437e81e7f3912a8ebbf184ea261a23
Anton Angelov <angelov.anton@yahoo.com>
2016-04-08 00:11:28 +0300

    AutomateThePlanetPoster --> add new github code beatifier service

---
+++ AutomateThePlanetPoster/AutomateThePlanetPoster/Core/GitHubCodeBeautifierService.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AutomateThePlanetPoster.Core
+{
+    public class GitHubCodeBeautifierService
+    {
+        public string Beautify(string urlAddress)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string beautifedHtml = string.Empty;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                beautifedHtml = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+            int indexOfSecondDocumentWrite = beautifedHtml.IndexOf("document.write('", 15);
+            System.Console.WriteLine(indexOfSecondDocumentWrite);
+            beautifedHtml = beautifedHtml.Substring(indexOfSecondDocumentWrite, beautifedHtml.Length - indexOfSecondDocumentWrite);
+
+            beautifedHtml = beautifedHtml.Replace("document.write('", string.Empty);
+            beautifedHtml = beautifedHtml.Replace("')", string.Empty);
+            beautifedHtml = beautifedHtml.Replace("\\n", string.Empty);
+            beautifedHtml = beautifedHtml.Replace("\\", string.Empty);
+            beautifedHtml = beautifedHtml.Replace("with &#10084; ", string.Empty);
+            beautifedHtml = beautifedHtml.Trim();
+
+            return beautifedHtml;
+        }
+    }
+}
--- AutomateThePlanetPoster/AutomateThePlanetPoster/Views/MainView.xaml.cs
+++ AutomateThePlanetPoster/AutomateThePlanetPoster/Views/MainView.xaml.cs
@@ -43,5 +43,18 @@ namespace AutomateThePlanetPoster.Views
                 MessageBox.Show("The content was copied to your clipboard.");
             }
         }
+
+        private void btnBeautifyGitHubCode_Click(object sender, RoutedEventArgs e)
+        {
+            GitHubCodeBeautifierService beautifyService = new GitHubCodeBeautifierService();
+            // <script src="https://gist.github.com/angelovstanton/b7fb201d0965672f3bc2.js"></script>
+            string sciptHtml = tbGitHubJsUrl.Text;
+            string url = sciptHtml.Replace("<script src=\"", string.Empty);
+            url = url.Replace("\"></script>", string.Empty); 
+            string generatedContent = beautifyService.Beautify(url);
+            Clipboard.SetText(generatedContent);
+            tbGitHubJsUrl.Text = string.Empty;
+            MessageBox.Show("The content was copied to your clipboard.");
+        }
     }
 }
